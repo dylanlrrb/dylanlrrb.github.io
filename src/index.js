46c6578e2da6d4458576d79ac170bfbfcac13d83
Dylan Larrabee <dylanlarrabee6@gmail.com>
2022-02-11 23:10:48 -0800

    add project popups

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
+projects = ["./src/projects/mnist-demo.html",
+            "./src/projects/supervised_learning.html",
+            "./src/projects/supervised_learning.html",
+            "./src/projects/supervised_learning.html",
+            "./src/projects/supervised_learning.html",
+            "./src/projects/supervised_learning.html"]
+
 window.addEventListener('DOMContentLoaded', (event) => {
-  hamburger_icon = document.querySelector('header.mobile .hamburger-icon')
-  x_icon = document.querySelector('header.mobile .x-icon')
-  mobile_nav = document.querySelector('header.mobile .nav')
-  selfie = document.querySelector('div.selfie')
+  const hamburger_icon = document.querySelector('header.mobile .hamburger-icon')
+  const mobile_nav = document.querySelector('header.mobile .nav')
+  const selfie = document.querySelector('div.selfie')
 
   hamburger_icon.addEventListener('click', () => mobile_nav.classList.toggle('closed'))
   mobile_nav.addEventListener('click', (e) => {
@@ -11,12 +17,39 @@ window.addEventListener('DOMContentLoaded', (event) => {
     }
   })
 
-  node = document.createElement('div')
+  const node = document.createElement('div')
   node.classList.add('transparent')
   node.classList.add('high')
   setTimeout(() => {
     selfie.appendChild(node)
   }, 100)
   setTimeout(() => node.classList.toggle('transparent'), 300)
+
+  Promise.all(projects.map(p => fetch(p).then(r => r.text())))
+    .then(data => {
+      data.forEach(d =>  document.querySelector("#projects").innerHTML += d)
+    })
+    .then(() => {
+      document.querySelectorAll('#projects .project')
+        .forEach(project => {
+          const tile = project.querySelector('.tile')
+          const modal = project.querySelector('.modal')
+          const exit = modal.querySelector('.exit')
+          const scrim = modal.querySelector('.modal-scrim')
+          tile.addEventListener('click', () => {
+            modal.classList.toggle('display-none')
+            document.documentElement.classList.toggle('disable-scroll')
+          })
+          exit.addEventListener('click', () => {
+            modal.classList.toggle('display-none')
+            document.documentElement.classList.toggle('disable-scroll')
+          })
+          scrim.addEventListener('click', () => {
+            modal.classList.toggle('display-none')
+            document.documentElement.classList.toggle('disable-scroll')
+          })
+        })
+    })
+
   
 });
