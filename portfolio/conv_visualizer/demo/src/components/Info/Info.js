11cd0e0fbb216f8260ed56755123857795bbef3c
dylanlrrb <dylan.r.larrabee@gmail.com>
2022-07-01 19:34:28 -0700

    add info section to projects

--- portfolio/conv_visualizer/demo/src/components/Info/Info.js
+++ portfolio/conv_visualizer/demo/src/components/Info/Info.js
@@ -20,6 +20,10 @@ const Info = () => {
       <div onClick={toggle} onTouchStart={toggle} className="Info-handle"></div>
       <div className="Info-content">
         <h2>Convolution Visualizer</h2>
+        <p>This demo visualizes how a selection of convolutional layers from a trained <a href="https://www.tensorflow.org/api_docs/python/tf/keras/applications/mobilenet" target="_blank" rel="noopener noreferrer">Mobilenet</a> extracts features from images taken from live video</p>
+        <p>You can see how, in higher layers, the filters of the CNN act rudementrey edge detectors and color extractors. Then in lower layers you the features extracted from the image become more abstract, blurrier, and temporatly stable as it finds patterns within patterns</p>
+        <p>Looking at it another way, a bright activated pixel in the filter of a high layer will represent someing simple like a blue corner or a dark edge. While a bright activated pixel in a filter of a lower layer will represent something much more abstract and complicated has been detected, such as an eye or a feather for example.</p>
+        <p>More info about this project's implementation can be found <a href="https://github.com/dylanlrrb/dylanlrrb.github.io/tree/master/portfolio/conv_visualizer" target="_blank" rel="noopener noreferrer">on Github</a></p>
       </div>
     </div>
     
--- portfolio/image_segmentation/demo/src/components/Info/Info.js
+++ portfolio/image_segmentation/demo/src/components/Info/Info.js
@@ -20,6 +20,9 @@ const Info = () => {
       <div onClick={toggle} onTouchStart={toggle} className="Info-handle"></div>
       <div className="Info-content">
         <h2>Common Object Image Segmentation</h2>
+        <p>This demo segments live video by attempting to assgn a class to each pixel and adding a colored overlay</p>
+        <p>The segmentation model is a U-Net with a MobilenetV2 model used as the downsample path. It was trained on the <a href="https://cocodataset.org/#home" target="_blank" rel="noopener noreferrer">COCO Dataset</a> to recognize and classify 80 diferent classes of common objects</p>
+        <p>More details on implementation can be found in the project repo <a href="https://github.com/dylanlrrb/dylanlrrb.github.io/tree/master/portfolio/image_segmentation" target="_blank" rel="noopener noreferrer">on Github</a></p>
       </div>
     </div>
     
