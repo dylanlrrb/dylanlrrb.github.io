c506df49443c6ccea09243652e485a2974e7e717
dylanlrrb <dylan.r.larrabee@gmail.com>
2022-07-13 16:04:03 -0700

    update sweep

--- portfolio/facial_keypoints/train.py
+++ portfolio/facial_keypoints/train.py
@@ -1,10 +1,6 @@
 import argparse
-import re
 import wandb
 import random
-
-
-
 
 
 hyperparameter_defaults = dict(
@@ -21,32 +17,33 @@ def main(project_dir, dataset_dir):
   wandb.init(config=hyperparameter_defaults, project="facial_keypoints_sweep", dir=PROJECT_DIR)
   config = wandb.config
 
+  print(config)
 
   wandb.log({'loss': random.randint(0,9)})
   print(project_dir, dataset_dir)
 
 
 if __name__ == '__main__':
-  # my_parser = argparse.ArgumentParser(description='Training script for facial keypoint CNN')
+  my_parser = argparse.ArgumentParser(description='Training script for facial keypoint CNN')
 
-  # my_parser.add_argument('--project_dir',
-  #                       '-p',
-  #                       type=str,
-  #                       required=False,
-  #                       help='path to the project relitive to /')
-  # my_parser.add_argument('--dataset_dir',
-  #                       '-d',
-  #                       '--dataset_dir',
-  #                       type=str,
-  #                       required=False,
-  #                       help='path to the dataset relitive to /')
+  my_parser.add_argument('--project_dir',
+                        '-p',
+                        type=str,
+                        required=False,
+                        help='path to the project relitive to /')
+  my_parser.add_argument('--dataset_dir',
+                        '-d',
+                        '--dataset_dir',
+                        type=str,
+                        required=False,
+                        help='path to the dataset relitive to /')
 
-  # args = my_parser.parse_args()
+  args = my_parser.parse_args()
 
-  # PROJECT_DIR = args.project_dir if args.project_dir != None else 'tf/notebooks/portfolio/facial_keypoints'
-  # DATASET_DIR = args.dataset_dir if args.dataset_dir != None else 'tf/notebooks/portfolio/facial_keypoints/data'
+  PROJECT_DIR = args.project_dir if args.project_dir != None else 'tf/notebooks/portfolio/facial_keypoints'
+  DATASET_DIR = args.dataset_dir if args.dataset_dir != None else 'tf/notebooks/portfolio/facial_keypoints/data'
 
-  PROJECT_DIR = 'tf/notebooks/portfolio/facial_keypoints'
-  DATASET_DIR = 'tf/notebooks/portfolio/facial_keypoints/data'
+  # PROJECT_DIR = 'tf/notebooks/portfolio/facial_keypoints'
+  # DATASET_DIR = 'tf/notebooks/portfolio/facial_keypoints/data'
 
   main(PROJECT_DIR, DATASET_DIR)
