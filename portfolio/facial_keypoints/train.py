27b42fddc73313a55674fddabdb0d009faaff463
dylanlrrb <dylan.r.larrabee@gmail.com>
2022-07-13 15:50:39 -0700

    try fixing argparser

--- portfolio/facial_keypoints/train.py
+++ portfolio/facial_keypoints/train.py
@@ -27,23 +27,26 @@ def main(project_dir, dataset_dir):
 
 
 if __name__ == '__main__':
-  my_parser = argparse.ArgumentParser(description='Training script for facial keypoint CNN')
+  # my_parser = argparse.ArgumentParser(description='Training script for facial keypoint CNN')
 
-  my_parser.add_argument('--project_dir',
-                        '-p',
-                        type=str,
-                        required=False,
-                        help='path to the project relitive to /')
-  my_parser.add_argument('--dataset_dir',
-                        '-d',
-                        '--dataset_dir',
-                        type=str,
-                        required=False,
-                        help='path to the dataset relitive to /')
+  # my_parser.add_argument('--project_dir',
+  #                       '-p',
+  #                       type=str,
+  #                       required=False,
+  #                       help='path to the project relitive to /')
+  # my_parser.add_argument('--dataset_dir',
+  #                       '-d',
+  #                       '--dataset_dir',
+  #                       type=str,
+  #                       required=False,
+  #                       help='path to the dataset relitive to /')
 
-  args = my_parser.parse_args()
+  # args = my_parser.parse_args()
 
-  PROJECT_DIR = args.project_dir if args.project_dir != None else 'tf/notebooks/portfolio/facial_keypoints'
-  DATASET_DIR = args.dataset_dir if args.dataset_dir != None else 'tf/notebooks/portfolio/facial_keypoints/data'
+  # PROJECT_DIR = args.project_dir if args.project_dir != None else 'tf/notebooks/portfolio/facial_keypoints'
+  # DATASET_DIR = args.dataset_dir if args.dataset_dir != None else 'tf/notebooks/portfolio/facial_keypoints/data'
+
+  PROJECT_DIR = 'tf/notebooks/portfolio/facial_keypoints'
+  DATASET_DIR = 'tf/notebooks/portfolio/facial_keypoints/data'
 
   main(PROJECT_DIR, DATASET_DIR)
